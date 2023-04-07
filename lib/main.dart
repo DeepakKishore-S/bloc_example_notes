cb0c1550fe859c245d2846f1c84ddcd5b920cc6f
Deepak Kishore S <sdkdeepak600@gmail.com>
2023-04-07 06:35:08 +0530

    example 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,6 +18,25 @@ class MyApp extends StatelessWidget {
           child: Text('Hello World'),
         ),
       ),
+    );
+  }
+}
+
+Stream<String> getTime() => Stream.periodic(Duration(seconds: 1), (_) {
+      return DateTime.now().toIso8601String();
+    });
+
+class HomePage extends HookWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final time = useStream(getTime());
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(time.data?? "no data"),
+      ),
+      body: Container(),
     );
   }
 }
