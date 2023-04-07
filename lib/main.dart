545d72de8667a4cf98c034eba824d8ec95dd81f4
Deepak Kishore S <sdkdeepak600@gmail.com>
2023-04-07 06:47:29 +0530

    example 2

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends HookWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useTextEditingController();
+    final text = useState("");
+    useEffect(() {
+      controller.addListener(() {
+        text.value = controller.text;
+      });
+      return null;
+    },[controller]);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(controller: controller,),
+            Text("You Typed ${text.value}")
+          ],
+        ),
+      ),
+    );
+  }
+}
